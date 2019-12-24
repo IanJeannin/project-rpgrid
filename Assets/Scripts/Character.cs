d03ce0802ae9984c3bba0ad21adc827b3c188995
Ian Jeannin <ian.jeannin@loop.colum.edu>
2019-12-24 18:49:02 -0500

    Add character script, limit movement based on Action Points
    Subtracts movement cost each time a new space is moved to.
    If moving to a space that has been moved to already, restores AP up to the amount that had been spent reaching that space originally.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [Tooltip("How much health the character starts with.")]
+    [SerializeField]
+    private float hitPoints=10;
+    [Tooltip("How many Action Points the character starts with.")]
+    [SerializeField]
+    private int actionPoints=5;
+
+    public int GetAP()
+    {
+        return actionPoints;
+    }
+
+    public void AdjustAP(int adjustment)
+    {
+        actionPoints = actionPoints + adjustment;
+    }
+}
