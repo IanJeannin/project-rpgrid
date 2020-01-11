07f381f00683b9faf34b68721ac2a73045b614fd
Ian Jeannin <ian.jeannin@loop.colum.edu>
2020-01-11 14:18:24 -0500

    Transfer RangeAttack to BasicAttack for naming purposes, as the attack could be melee

--- Assets/Scripts/BasicAttack.cs
+++ Assets/Scripts/BasicAttack.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RangeAttack : Attack
+public class BasicAttack : Attack
 {
     private Pathfinding pathfinding;
     private int range;
-    public RangeAttack(int newDamage,int newAccuracy, int newRange):base(newDamage,newAccuracy)
+
+    public BasicAttack(int newDamage, int newAccuracy, int newRange) : base(newDamage, newAccuracy)
     {
         damage = newDamage;
         accuracy = newAccuracy;
@@ -16,7 +17,7 @@ public class RangeAttack : Attack
     public void MakeAttack(Character attacker, Enemy defender)
     {
         int numberOfSpaces = pathfinding.CheckRange(attacker.transform.position, defender.transform.position);
-        if(numberOfSpaces<=range)
+        if (numberOfSpaces <= range)
         {
             MakeAttack(defender);
         }
